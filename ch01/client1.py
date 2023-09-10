626b6ad6e4734b4e8ebae1193f4493dcc496ab23
hyungsik74 <hyungsik.edu@gmail.com>
2023-09-10 13:44:17 +0900

    web client server python

--- ch01/client1.py
+++ ch01/client1.py
@@ -1,8 +1,8 @@
 import socket
   
 mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-mysock.connect(('hyungsik74.github.io', 80))
-cmd = 'GET http://hyungsik74.github.io/django/ch01/page01.html HTTP/1.0\r\n\r\n'.encode()
+mysock.connect(('127.0.0.1', 9000))
+cmd = 'GET http://127.0.0.1/ HTTP/1.0\r\n\r\n'.encode()
 mysock.send(cmd)
 
 while True:
